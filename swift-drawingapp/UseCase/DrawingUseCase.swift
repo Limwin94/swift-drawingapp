2a2acd278da18ba8f423f0a3315002f7ad622d5d
임승혁 <seunghyeok.lim@kurlycorp.com>
2022-10-20 00:21:42 +0900

    feature: UseCase 작성

---
+++ swift-drawingapp/UseCase/DrawingUseCase.swift
@@ -0,0 +1,19 @@
+//
+//  DrawingUseCase.swift
+//  swift-drawingapp
+//
+//  Created by MK-AM16-010 on 2022/10/19.
+//
+
+import Foundation
+
+final class DrawingUseCase {
+  
+  // 사각형, 드로잉 타입을 선택한다.
+  func changeType(_ currentType: DrawingType) -> DrawingType {
+    var type = currentType
+    type.toggle()
+    
+    return type
+  }
+}
