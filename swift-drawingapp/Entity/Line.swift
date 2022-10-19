f396d34943ffc602001eeff12a805b87b22022d8
임승혁 <seunghyeok.lim@kurlycorp.com>
2022-10-19 23:17:53 +0900

    feature: Entity(사각형, 라인) 및 protocol 추가

---
+++ swift-drawingapp/Entity/Line.swift
@@ -0,0 +1,12 @@
+//
+//  Line.swift
+//  swift-drawingapp
+//
+//  Created by MK-AM16-010 on 2022/10/19.
+//
+
+import UIKit
+
+struct Line: RandomColorRenerator {
+  var paths: [CGPoint]
+}
---
+++ swift-drawingapp/Entity/Rectangle.swift
@@ -0,0 +1,13 @@
+//
+//  Rectangle.swift
+//  swift-drawingapp
+//
+//  Created by MK-AM16-010 on 2022/10/19.
+//
+
+import UIKit
+
+struct Rectangle: RandomColorRenerator {
+  let width: UInt = 100
+  let height: UInt = 100
+}
